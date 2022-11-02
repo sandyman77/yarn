a942fcbe730362862ca4ac911b810050305a6e33
Sander <sandyman77@protonmail.com>
2022-11-02 18:42:42 +0100

    reading yarn.lock

--- main.go
+++ main.go
@@ -1,18 +1,63 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "log"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 )
 
+type Lockfile struct {
+	Packages []string
+}
+
 func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Missing path argument")
+	}
+	yarn := os.Args[1]
+	content, err := ioutil.ReadFile(yarn)
+	contentString := string(content)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    content, err := ioutil.ReadFile("testdata/yarn.lock")
+	listOfLines := strings.Split(contentString, "\n")[4:]
+	//lockfile := Lockfile{Packages: []string{}}
+	amountOfLines := len(listOfLines)
+	for i := 0; i < amountOfLines; {
+		line := listOfLines[i]
+		if len(line) > 2 && line[:2] != "  " && string(line[len(line)-1]) == ":" {
+			fmt.Println("package found", line)
+		} else if line != "" {
+			items := strings.Split(line[2:], " ")
+			if items[0] == "dependencies:" {
+				fmt.Println("\nDependencies found")
+				count := getDependencies(listOfLines[i+1:])
+				i += count
+			} else {
+				fmt.Println("key ", items[0])
+				fmt.Println("value ", items[1])
+			}
+		} else {
+			fmt.Println("Package ended.\n")
+		}
+		i += 1
+	}
+}
 
-     if err != nil {
-          log.Fatal(err)
-     }
-
-    fmt.Println(string(content))
+func getDependencies(lines[]string) int {
+	count := 0
+	for _, line := range lines {
+		if string(line) == "" {
+			break
+		} else {
+			items := strings.Split(string(line)[4:], " ")
+			fmt.Println("Dependency name:", items[0])
+			fmt.Println("Dependency version:", items[1])
+			count++
+		}
+	}
+	return count
 }
